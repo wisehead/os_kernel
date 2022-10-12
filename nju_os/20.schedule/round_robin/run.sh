ec5cfe3bf25c5e2c51ccad3e3538d13339fc374f
wisehead <alex.chenhui@gmail.com>
2022-10-12 15:09:50 +0800

    mandelbrot.c

---
+++ nju_os/20.schedule/round_robin/run.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: run.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/10/12-15:09:24
+#############################################################
+#!/bin/sh 
+gcc mandelbrot.c -O2 -lpthread -lm
